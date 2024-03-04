d93cf3c33bc32462e761c8b11b6f3398146e57af
avzotova_41 <avzotova_4@edu.hse.ru>
2024-03-04 22:39:51 +0300

    fix

--- lab-09_figures/src/Rectangle.cpp
+++ lab-09_figures/src/Rectangle.cpp
@@ -10,8 +10,7 @@ void Rectangle::print() const {
 }
 
 bool Rectangle::is_inside(int x, int y) const {
-    return (x <= this->x + width/2 && x >= this->x - width/2 && y <= this->y + height/2 && y >= this->y - height/2) 
-
+    return (x <= this->x + width/2 && x >= this->x - width/2 && y <= this->y + height/2 && y >= this->y - height/2); 
 }
 
 void Rectangle::zoom(int factor) {
